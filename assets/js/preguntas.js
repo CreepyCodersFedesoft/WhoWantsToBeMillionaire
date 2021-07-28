49b5850b18d8b31a6d677636de4f3a9c64b43e09
Diego R <alejorvela@gmail.com>
2021-07-28 11:05:13 -0500

    first commit

---
+++ assets/js/preguntas.js
@@ -0,0 +1,11 @@
+let  pregunta = {
+    titulo : "de color es el cielo",
+    opciones : [
+        "verde",
+        "amarillo",
+        "azul"
+    ],
+    correcto: 2
+}
+
+let preguntas = [pregunta];

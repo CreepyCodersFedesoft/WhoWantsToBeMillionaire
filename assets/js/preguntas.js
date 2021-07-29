a71ebd5105c0cd3309a765ac5f401bbdcb2e2ee7
Sebastian Tibaquirá <johnsebas94@gmail.com>
2021-07-29 09:55:00 -0500

    Se suben preguntas en archivo preguntas.js

--- assets/js/preguntas.js
+++ assets/js/preguntas.js
@@ -1,12 +1,108 @@
-let  pregunta = {
-    titulo : "de color es el cielo",
-    opciones : [
-        "verde",
-        "amarillo",
-        "naranja",
-        "azul"
+let preguntas = [
+  {
+    titulo:
+      "¿Cual de los siguientes lenguajes, no es usado para el desarrollo web?",
+    opciones: ["Css", "Html", "Visual Basic", "JavaScript"],
+    correcto: 2,
+  },
+  {
+    titulo:
+      "¿Cuál de las siguientes sentencias se usa para la codificación de un condicional?",
+    opciones: ["If", "Switch", "Function", "Conditional"],
+    correcto: 0,
+  },
+  {
+    titulo: "¿Qué etiqueta es usada para invocar un archivo JS en un HTML?",
+    opciones: ["<button>", "<a>", "<div>", "<script>"],
+    correcto: 3,
+  },
+  {
+    titulo: "¿Cuál de las características no son importantes para Docker?",
+    opciones: [
+      "Modelo-Vista-Controlador",
+      "Agilidad de aplicación",
+      "Productividad del desarrollador",
+      "Eficiencias operativas",
     ],
-    correcto: 2
-}
-
-let preguntas = [pregunta];
+    correcto: 0,
+  },
+  {
+    titulo:
+      "¿Qué comando se ejecuta para ver todos los contenedores en ejecución en Docker?",
+    opciones: ["docker list", "docker up", "docker ps", "docker down"],
+    correcto: 2,
+  },
+  {
+    titulo: "¿Cuál es el comando para detener un contenedor de Docker?",
+    opciones: [
+      "docker nombre_contenedor stop",
+      "docker stop nombre_contenedor",
+      "docker -f stop nombre_contenedor",
+      "docker -f nombre_contenedor stop",
+    ],
+    correcto: 1,
+  },
+  {
+    titulo: "¿Qué es git?",
+    opciones: [
+      "Lenguaje de programación.",
+      "Base de datos",
+      "Framework",
+      "Sistema de control de versiones",
+    ],
+    correcto: 3,
+  },
+  {
+    titulo: "¿Quién fue el diseñador de git?",
+    opciones: ["Linus Torvalds", "Will Smith", "Alan Turing", "Thomas Edison"],
+    correcto: 0,
+  },
+  {
+    titulo:
+      "¿Cual es el comando usado para subir cambios a un repositorio en la web?",
+    opciones: ["git push", "git commit", "git add .", "git pull"],
+    correcto: 0,
+  },
+  {
+    titulo: "¿Qué es SEO?",
+    opciones: [
+      "System energy operator",
+      "Systematic Enterprise Organization",
+      "Search Engine Optimization",
+      "Site Enrollment Online",
+    ],
+    correcto: 2,
+  },
+  {
+    titulo: "¿No es un método de HTTP?",
+    opciones: ["GET", "NEXT", "PUT", "HEAD"],
+    correcto: 1,
+  },
+  {
+    titulo: "¿En dónde se encuentra la capa de negocios?",
+    opciones: [
+      "Back-end",
+      "Front-end",
+      "Bases de datos",
+      "Se puede ubicar donde el desarrollador lo vea pertinente.",
+    ],
+    correcto: 0,
+  },
+  {
+    titulo: "¿Es una sub-variante de Red-Hat?",
+    opciones: ["Ubuntu", "Arch Linux", "Kali", "Fedora"],
+    correcto: 3,
+  },
+  {
+    titulo:
+      "¿Cual de los siguientes comandos me permite hacer búsquedas solo por el nombre del archivo en Linux?",
+    opciones: ["grep", "locate", "find", "search"],
+    correcto: 1,
+  },
+  {
+    titulo:
+      "¿Cual de los siguientes comandos me permite visualizar los archivos y carpetas ocultas en Linux?",
+    opciones: ["ls-l", "ls-r", "ls-a", "ls -t"],
+    correcto: 2,
+  },
+];

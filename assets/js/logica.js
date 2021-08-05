01389a195e51034154d83ff8327975307e0f3fac
Diego R <alejorvela@gmail.com>
2021-08-05 10:39:52 -0500

    coding videogame

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -8,22 +8,30 @@ let $btnA3 = document.getElementById("btnA3");
 let $btnA4 = document.getElementById("btnA4");
 let currentLevel = 0;
 let asnwerRight = -1;
-let currentAsnwer = "";
+let currentAsnwer = -1;
+let currentID = "";
 
 //cueando comience el nivel, debe seleccionarse el nivel en que quedo y la puntuacion
 //desde el local storage, y comenzar a correr el cronometro
 const startLevel = () => {
-    if(localStorage.getItem('level') && localStorage.getItem('level') !== "" && localStorage.getItem('points') && localStorage.getItem('points') !== ""){
+    
+    resetTime();
+    if (
+        localStorage.getItem("level") &&
+        localStorage.getItem("level") !== "" &&
+        localStorage.getItem("points") &&
+        localStorage.getItem("points") !== ""
+    ) {
         loadLevel(localStorage.getItem("level"), localStorage.getItem("points"));
     } else {
         //iniciando nueva partida
         loadLevel("1", "0");
     }
-}
-const loadLevel = (level, points) =>{
-    localStorage.setItem('level', level);
-    localStorage.setItem('points', points);
-    
+};
+const loadLevel = (level, points) => {
+    localStorage.setItem("level", level);
+    localStorage.setItem("points", points);
+
     //enviamos preguntas
     level = parseInt(level) - 1;
     console.log(level);
@@ -34,45 +42,109 @@ const loadLevel = (level, points) =>{
     $btnA3.innerHTML = preguntas[level]["opciones"][2];
     $btnA4.innerHTML = preguntas[level]["opciones"][3];
     asnwerRight = preguntas[level]["correcto"];
+    console.log("nivel cargado, la respuesta correcta es " + asnwerRight);
 };
 
 const nextLevel = () => {
     let level = parseInt(localStorage.getItem("level"));
-    let poinst = localStorage.getItem("points");
-    //validar si la pregunta esta bien
-    
-    //let response = pregunta["correcto"]
-    //pregunta["opciones"][response]
-    
-    
-    //cargamos siguiente nivel
-    level === preguntas.length ? alert(`felicidades, ganeste y tu puntaje fue ${poinst}`) : loadLevel(String(level+1), poinst);
+    let poinst = parseInt(localStorage.getItem("points"));
+
+    //validar si no ha seleccionado una pregunta
+    if (currentAsnwer !== -1) {
+        console.log(
+            "respuesta seleccionada " + currentAsnwer + " r. correcta " + asnwerRight
+        );
+        if (asnwerRight === currentAsnwer) {
+            //sumamos puntos y cambiamos color del boton
+            poinst += 100;
+            document.getElementById(currentID).style.background = "#18C41D";
+            //deshabilitamos el boton de next mientras se realiza la transición
+            document.getElementById("btnNext").disabled=true;
+
+            resetTime();
+
+
+            //cargamos siguiente nivel
+            let t = 2;
+            let g = setInterval(function () {
+                console.log(t);
+                if (t === 0) {
+                    //vaciar la pregunta seleccionada
+                    cleanAnswer();
+
+                    //cargamos el siguiente nivel
+                    document.getElementById(currentID).style.background = "#001980";
+                    currentID = "";
+                    level === preguntas.length
+                        ? alert(`felicidades, ganaste y tu puntaje fue ${poinst}`)
+                        : loadLevel(String(level + 1), poinst);
+                    document.getElementById("btnNext").disabled=false;
+                    resetTime();
+                    clearInterval(g);
+                }
+                t--;
+            }, 1000);
+        } else {
+            document.getElementById(currentID).style.background = "#E71A0D";
+            document.getElementById("btnNext").disabled=true;
+            let t = 2;
+            let g = setInterval(function () {
+                console.log(t);
+                if (t === 0) {
+                    document.getElementById(currentID).style.background = "#001980";
+                    //alert("Has perdido");
+                    cleanStorage();
+                    document.getElementById("btnNext").disabled=false;
+                    startLevel();
+                    clearInterval(g);
+                }
+                t--;
+            }, 1000);
+        }
+    } else {
+        alert("respuesta vacia");
+    }
 };
 
 const cleanStorage = () => {
     localStorage.clear();
+    currentID = "";
+    currentAsnwer = -1;
+    asnwerRight = -1;
+};
+const cleanAnswer = () => {
+    currentAsnwer = -1;
+    asnwerRight = -1;
 };
 
-const getTextButton = (text) => {
-    currentAsnwer = text;
+const getTextButton = (index) => {
+    currentAsnwer = index;
     console.log(`La respuesta seleccionada actualmente es ${currentAsnwer}`);
 };
 
+const sendID = (idGotIt) => {
+    currentID !== ""
+        ? (document.getElementById(currentID).style.background = "#001980"): console.log("no hay currentID");
+        
+    currentID = idGotIt.id;
+    (document.getElementById(idGotIt.id).style.background = "#C3C32C")
+    getTextButton(parseInt(currentID.slice(-1)) - 1);
+};
 
 startLevel();
-
+StartTime();
 
 $btnA1.onclick = function () {
-    getTextButton($btnA1.innerText)
+    sendID($btnA1);
 };
 $btnA2.onclick = function () {
-    getTextButton($btnA2.innerText)
+    sendID($btnA2);
 };
 $btnA3.onclick = function () {
-    getTextButton($btnA3.innerText)
+    sendID($btnA3);
 };
 $btnA4.onclick = function () {
-    getTextButton($btnA4.innerText)
+    sendID($btnA4);
 };
 $btnNext.onclick = function () {
     nextLevel();
--- assets/js/tiempo.js
+++ assets/js/tiempo.js
@@ -1,7 +1,24 @@
-tiempo = document.getElementById("tiempo");
-n=60;
-x = setInterval(function(){
-    tiempo.innerHTML=n;
-    if(n==0){ alert("Se te acabo el tiempo"); clearInterval(x); }
-    n--;
-},1000);
+let tiempo = document.getElementById("tiempo");
+let defaultTime = 60;
+let timeToLose = defaultTime;
+let StartTime = () => {
+    x = setInterval(function(){
+        tiempo.innerHTML=timeToLose;
+        if(timeToLose==0){ 
+            alert("¡Has perdido!, el tiempo se ha agotado..."); 
+            cleanStorage();
+            document.getElementById("btnNext").disabled=false;
+            startLevel();
+            resetTime();
+            StartTime();
+            clearInterval(x); 
+        }
+        timeToLose--;
+    },1000);
+}
+
+
+
+const resetTime = () => {
+    timeToLose = defaultTime;
+};
